f6c0ead84be230c97929af8ee6a29bd4b4e3b6e1
afliw <aflivv@gmail.com>
2019-04-19 21:51:06 +0100

    Revamp code

--- index.js
+++ index.js
@@ -1,120 +1,177 @@
 /*jshint esversion: 9 */
+///<reference path="index.d.ts" />
 
-var path = require("path");
-var fs = require('fs');
+const path = require("path");
+const fs = require('fs');
 
-var Router = {
-    defaultFilename: "index.html",
-    publicDirectory: undefined,
-    fileNotFound: undefined,
-    fallbackResource: undefined,
-    routes: {
-        GET: {},
-        POST: {},
-        PUT: {},
-        PATCH: {},
-        DELETE: {},
-        HEAD: {},
-        TRACE: {},
-        CONNECT: {},
-        OPTIONS: {}
-    },
-    get: function(url, callback){
+class Router {
+    constructor(options) {
+        options = options || {};
+        this.routes = {
+            GET: {},
+            POST: {},
+            PUT: {},
+            PATCH: {},
+            DELETE: {},
+            HEAD: {},
+            TRACE: {},
+            CONNECT: {},
+            OPTIONS: {}
+        };
+        this.publicDirectory = options.publicDirectory;
+        this.fallback = options.fallback;
+        this.defaultFilename = options.defaultFilename;
+    }
+    get(url, callback) {
         this.routes.GET[url] = callback;
-    },
-    post: function(url, callback){
+    }
+    post(url, callback) {
         this.routes.POST[url] = callback;
-    },
-    put: function(url, callback) {
+    }
+    put(url, callback) {
         this.routes.PUT[url] = callback;
-    },
-    patch: function(url, callback) {
+    }
+    patch(url, callback) {
         this.routes.PATCH[url] = callback;
-    },
-    delete: function(url, callback) {
+    }
+    delete(url, callback) {
         this.routes.DELETE[url] = callback;
-    },
-    route: function(request, response){
-        request.on("data", (d) => onRequestData(request, d));
-        request.on("end", () => onRequestEnd(request, response));
-    },
+    }
+    route() {
+        return function(request, response) {
+            request.on("data", (d) => onRequestData(request, d));
+            request.on("end", () => onRequestEnd(request, response, this));
+        }.bind(this);
+    }
     setDefaultFilename(name) {
         this.defaultFilename = name;
-    },
-    setFallback(resource) {
-        this.fallbackResource = resource;
-    },
+    }
+    setFallback(callback) {
+        this.fallback = callback;
+    }
     setPublicDirectory(path) {
-        if(typeof path !== "string") throw new Error("Argument 'path' must be of type 'string'");
+        if (typeof path !== "string")
+            throw new Error("Argument 'path' must be of type 'string'");
         this.publicDirectory = path;
-    },
+    }
     removeRoute(url, method) {
-        if(!method) {
-            if(this.routes[method.toUpperCase()].hasOwnProperty(url)) return false;
-            delete this.this.routes[method.toUpper()][url];
+        if (!method) {
+            if (this.routes[method.toUpperCase()].hasOwnProperty(url))
+                return false;
+            delete this.routes[method.toUpper()][url];
         }
-        for(let met in this.routes){
-            if(this.routes[met].hasOwnProperty(url)) delete this.this.routes[m][url];
+        for (let met in routes) {
+            if (this.routes[met].hasOwnProperty(url))
+                delete this.routes[m][url];
         }
         return true;
-    },
+    }
     newRoute(url, actions) {
-        for(let method in actions) {
-            if(!this.routes.hasOwnProperty(method.toUpperCase())) {
+        for (let method in actions) {
+            if (!this.routes.hasOwnProperty(method.toUpperCase())) {
                 console.warn(`Invalid method '${method}' in route definition.`);
                 continue;
             }
             this.routes[method.toUpperCase()][url] = actions[method];
         }
-    },
-    listen: function(port) {
-        if(!port) throw "Must specify a port to listen to.";
+    }
+    listen(port) {
+        if (!port)
+            throw "Must specify a port to listen to.";
         const http = require("http");
-        return http.createServer(this.route).listen(port);
-    } 
-};
+        return http.createServer(this.route()).listen(port);
+    }
+}
 
-function ResponseShortands(originalResponse) {
+class ResponseShortands {
 
-    var response = originalResponse;
-
-    function genericAnswer(data, opts) {
-        response.writeHead(opts.statusCode || 200, opts.statusText, opts.headers);
-        response.end(data);
+    constructor(response) {
+        this.response = response;
     }
 
-    function parseOptions(opts, contentType) {
+    generic(data, opts) {
+        this.response.writeHead(opts.statusCode || 200, opts.statusText || "", opts.headers);
+        this.response.end(data);
+    }
+
+    parseOptions(opts, contentType) {
         opts = opts || {};
         opts.headers = opts.headers || {};
         opts.headers["Content-Type"] = opts.headers["Content-Type"] || contentType;
         return opts;
     }
 
-    return {
-        json(data, opts) {
-            opts = parseOptions(opts, "application/json");
-            data = typeof data == "string" ? data : JSON.stringify(data);
-            genericAnswer(data, opts);
-        },
-        text(data, opts) {
-            opts = parseOptions(opts, "text/plain");
-            data = typeof data == "string" ? data : JSON.stringify(data);
-            genericAnswer(data, opts);
-        },
-        html(data, opts) {
-            opts = parseOptions(opts, "text/html");
-            genericAnswer(data, opts);
-        },
-        file(path, opts) {
-            if(!fs.existsSync(path)) throw `Error: file '${path}' doesn't exists.`;
-            var mime = getMimeType(getFileExtension(path));
-            fs.readFile(path, (err, content) => {
-                if(err) throw err.message;
-                opts = parseOptions(opts, mime);
-                genericAnswer(content, opts);
-            });
+    json(data, opts) {
+        opts = this.parseOptions(opts, "application/json");
+        data = typeof data == "string" ? data : JSON.stringify(data);
+        this.generic(data, opts);
+    }
+    text(data, opts) {
+        opts = this.parseOptions(opts, "text/plain");
+        data = typeof data == "string" ? data : JSON.stringify(data);
+        this.generic(data, opts);
+    }
+    html(data, opts) {
+        opts = this.parseOptions(opts, "text/html");
+        this.generic(data, opts);
+    }
+    file(path, opts) {
+        if(!fs.existsSync(path)) throw `Error: file '${path}' doesn't exists.`;
+        var mime = MimeTypes.findByExtension(getFileExtension(path));
+        fs.readFile(path, (err, content) => {
+            if(err) throw err.message;
+            opts = this.parseOptions(opts, mime);
+            this.generic(content, opts);
+        });
+    }
+}
+
+function handleRequest(parsedRequest, router) {
+    parsedRequest.fileExtension = getFileExtension(parsedRequest.url);
+    var routeCallback = matchRoute(router.routes[parsedRequest.method], parsedRequest.url);
+    if (routeCallback) {
+        parsedRequest.namedParameters = routeCallback.params;
+        routeCallback.method(parsedRequest, parsedRequest.__HTTP_Response__);
+    }
+    else if (parsedRequest.fileExtension) {
+        returnStaticContent(parsedRequest, router);
+    }
+    else {
+        if (router.defaultFilename && 
+            router.publicDirectory && 
+            fs.existsSync(path.join(router.publicDirectory, parsedRequest.url, router.defaultFilename))) {
+            parsedRequest.url += router.defaultFilename;
+            returnStaticContent(parsedRequest);
         }
-    };
+        else {
+            fallbackToResource(parsedRequest, router);
+        }
+    }
+}
+function fallbackToResource(parsedRequest, router) {
+    let action = router.fallbackResource;
+    if (!action) {
+        return parsedRequest.answer.text("File or action not found on this server.", { statusCode: 404, statusText: "Not found" });
+    }
+    if (typeof action == "string") {
+        if (!fs.existsSync(action))
+            throw `File ${action} could not be found.`;
+        parsedRequest.answer.file(action);
+    }
+    else if (typeof action == "function") {
+        action(parsedRequest, parsedRequest.__HTTP_Response__);
+    }
+    else {
+        throw `Fallback resource '${action}' is not a file or a callback.`;
+    }
+}
+function returnStaticContent(parsedRequest, router) {
+    if (router.publicDirectory === undefined)
+        return fallbackToResource(parsedRequest, router);
+    var fullPath = path.join(router.publicDirectory, parsedRequest.url);
+    if (!fs.existsSync(fullPath))
+        return fallbackToResource(parsedRequest, router);
+    parsedRequest.answer.file(fullPath);
 }
 
 function onRequestData(request, data) {
@@ -122,7 +179,7 @@ function onRequestData(request, data) {
     request.body += data.toString("binary");
 }
 
-function onRequestEnd(request, response) {
+function onRequestEnd(request, response, router) {
     var decomposedUrl = parseUrl(request.url);
     let parsedRequest = {
         body: parseBody(request) || {},
@@ -141,20 +198,20 @@ function onRequestEnd(request, response) {
     };
     Object.defineProperty(parsedRequest, "__HTTP_Request__", {descriptor, value: request});
     Object.defineProperty(parsedRequest, "__HTTP_Response__", {descriptor, value: response});
-    var rs = ResponseShortands(response);
+    var rs = new ResponseShortands(response);//ResponseShortands(response);
     Object.defineProperty(parsedRequest, "answer", {...descriptor, value: rs});
-    handleRequest(parsedRequest);
+    handleRequest(parsedRequest, router);
 }
 
 function parseUrl(url) {
     var urlParts = url.split("?");
     return {
         url: urlParts[0],
-        queryParams: urlParts.length > 1 ? getQueryParams(urlParts[1]) : {}
+        queryParams: urlParts.length > 1 ? parseURLEncodedParameters(urlParts[1]) : {}
     };
 }
 
-function getQueryParams(queryString) {
+function parseURLEncodedParameters(queryString) {
     var queryObj = {};
     var queryStringParts = queryString.split("&");
     for (var i = 0; i < queryStringParts.length; i++) {
@@ -179,58 +236,23 @@ function castValue(val) {
     return parseFloat(val) || val;
 }
 
+const MimeTypes =  {
+    mimes: JSON.parse(fs.readFileSync("mimes.json")),
+    
+    findByExtension(ext) {
+        mime = this.mimes.find( m => {
+            return m.ext == ext;
+        });
+        return mime && mime.type || "text/plain";
+    },
 
-function fallbackToResource(parsedRequest) {
-    let action = Router.fallbackResource;
-    if(!action) {
-        return parsedRequest.answer.text("File or action not found on this server.", {statusCode: 404, statusText: "Not found"});
+    findByMime(mime) {
+        ext = this.mimes.find( m => {
+            return m.type == mime || m.alt && m.alt.find( a => a == mime);
+        });
+        return ext && ext.ext || undefined;
     }
-    if(typeof action == "string") {
-        if(!fs.existsSync(action))
-            throw `File ${action} could not be found.`;
-        parsedRequest.answer.file(action);
-    } else if(typeof action == "function"){
-        action(parsedRequest, parsedRequest.__HTTP_Response__);
-    } else {
-        throw `Fallback resource '${action}' is not a file or a callback.`;
-    }
-}
-
-function getMimeType(ext) {
-    var MimeTypes = {
-        js: "application/javascript",
-        css: "text/css",
-        jpg: "image/jpeg",
-        html: "text/html",
-        mp4: "video/mpeg",
-        txt: "text/plain",
-        png: "image/png",
-        gif: "image/gif",
-        json: "application/json",
-        woff2: "font/woff2",
-        mp3: "audio/mpeg3",
-        woff: "application/font-woff"
-    };
-    return MimeTypes.hasOwnProperty(ext) ? MimeTypes[ext] : "text/html";
-}
-
-function handleRequest(parsedRequest) {
-    parsedRequest.fileExtension = getFileExtension(parsedRequest.url);
-    var routeCallback = matchRoute(Router.routes[parsedRequest.method], parsedRequest.url);
-    if (parsedRequest.fileExtension) {
-        returnStaticContent(parsedRequest);
-    } else if (routeCallback) {
-        parsedRequest.namedParameters = routeCallback.params;        
-        routeCallback.method(parsedRequest, parsedRequest.__HTTP_Response__);
-    } else {
-        if(Router.defaultFilename && fs.existsSync(path.join(Router.publicDirectory, parsedRequest.url, Router.defaultFilename))) {
-            parsedRequest.url += Router.defaultFilename;
-            returnStaticContent(parsedRequest);
-        } else {
-            fallbackToResource(parsedRequest);
-        }
-    }
-}
+};
 
 function matchRoute(routes, url) {
     if(!routes) return false;
@@ -240,15 +262,21 @@ function matchRoute(routes, url) {
     } else {
         var candidates = Object.keys(routes).filter(r => /{[^\/]+}/.test(r));
         for(let candidate of candidates) {
-            let splitRoute = candidate.split("/");
-            let regexUrl = splitRoute.reduce( (a, c) => {
-                let test = /{(?<param>.*)}/.exec(c);
-                c = test ? `(?<${test.groups.param}>[^\\/]+)` : c;
-                return a + "\\/" + c;
-            });
+            // let splitRoute = candidate.split("/");
+            // let regexUrl = splitRoute.reduce( (a, c) => {
+            //     let test = /{(?<param>.*)}/.exec(c);
+            //     c = test ? `(?<${test.groups.param}>[^\\/]+)` : c;
+            //     return a + "\\/" + c;
+            // });
+            let paramMatch = null;
+            let regexUrl = candidate;
+            var paramRegex =  /{(?<param>[a-z0-9]+)}/ig;
+            while((paramMatch = paramRegex.exec(candidate))) {
+                regexUrl = regexUrl.replace(paramMatch[0], `(?<${paramMatch[1]}>[^\\/]+)`);
+            }
             regexUrl = new RegExp("^" + regexUrl + "$", "i");
             if(regexUrl.test(url)){
-                var match = regexUrl.exec(url);
+                let match = regexUrl.exec(url);
                 return {method: routes[candidate] , params: match.groups};
             }
         }
@@ -256,7 +284,8 @@ function matchRoute(routes, url) {
 }
 
 function getFileExtension(url){
-    var rx = /\.(js|css|jpg|html|mp4|txt|png|gif|json|woff2|mp3)$/;
+    //var rx = /\.(js|css|jpg|html|mp4|txt|png|gif|json|woff2|mp3)$/;
+    var rx = /\.([a-zA-Z0-9]+)$/;
     var match = rx.exec(url);
     return match ? match[1] : false;
 }
@@ -279,6 +308,7 @@ function parseBody(request){
             params = parseFormData(request.body);
             break;
         default:
+            params = {_raw: request.body};
             break;
     }
     return params;
@@ -293,22 +323,38 @@ function parseFormData(data){
         var name = match[2];
         var filename = match[3];
         var mime = match[4];
-        formData[name] = filename ? {
-            filename: filename,
-            file: val,
-            mime: mime
-        } : val;
+        formData[name] = filename ? new InputFile(filename, mime, val) : val;
         //if (filename) fs.writeFileSync(path.join(__dirname, "temp", filename), val, "binary");
         match = rx.exec(data);
     }
     return formData;
 }
 
-function returnStaticContent(parsedRequest) {
-    if(Router.publicDirectory === undefined) return fallbackToResource(parsedRequest);
-    var fullPath = path.join(Router.publicDirectory, parsedRequest.url);
-    if(!fs.existsSync(fullPath)) return fallbackToResource(parsedRequest);
-    parsedRequest.answer.file(fullPath);
+class InputFile {
+    constructor(filename, mime, content) {
+        this.filename = filename;
+        this.mime = mime;
+        this.content = content;
+        this.extension = filename.replace(/.*\.([^.]+)$/, "$1");
+        this.extensionFromMime = MimeTypes.findByMime(mime);
+    }
+
+    save(path, encoding, callback) {
+        callback = typeof encoding == "function" ? encoding : callback;
+        ecoding = typeof encoding == "string" ? encoding : "binary";
+        fs.writeFile(path, this.content, encding, callback);
+    }
+
+    saveSync(path, encoding) {
+        fs.writeFileSync(path, this.content, encoding || "binary");
+    }
+
+    destroy() {
+        delete this.filename;
+        delete this.mime;
+        delete this.content;
+        delete this.extension;
+    }
 }
 
 module.exports = Router;
